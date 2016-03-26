3d1c07014fe580493253219c4ebebffeb97878eb
johnlinp <johnlinp@gmail.com>
2016-03-26 21:24:22 +0800

    Execute when "agogo" is found in the url or title

---
+++ content/browserOverlay.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function load(event){
+    window.removeEventListener('load', load, false);
+    AgogoChase.init();
+}, false);
+
+var AgogoChase = {
+    init: function() {
+        var appcontent = document.getElementById('appcontent');
+        if (appcontent){
+            appcontent.addEventListener('DOMContentLoaded', AgogoChase.onPageLoad, true);
+        }
+    },
+
+    onPageLoad: function(evt) {
+        var doc = evt.originalTarget;
+        var keyword = 'agogo';
+        var url = doc.location.href;
+        var title = doc.title;
+
+        if (url.toLowerCase().search(keyword) == -1 &&
+                title.toLowerCase().search(keyword) == -1) {
+            return;
+        }
+
+        alert('show agogo here');
+    },
+};
