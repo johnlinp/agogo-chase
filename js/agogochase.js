9a9baf758b8efcaddf0bf3b78caad5b5777301f4
johnlinp <johnlinp@gmail.com>
2016-03-27 21:34:25 +0800

    Drop grass and eat it

--- js/agogochase.js
+++ js/agogochase.js
@@ -5,6 +5,8 @@
     closeDistance: 30, // in pixels, for agogo
     fallDistance: 60, // in pixels, for grass
     fallSpeed: 5, // in pixels, for grass
+    reactGrassTime: 2000, // in milliseconds
+    eatingTime: 5000, // in milliseconds
     state: 'init',
     restPlace: null,
     curTarget: null,
@@ -33,6 +35,7 @@
         var me = this;
         document.addEventListener('mouseup', function(evt) {
             me.makeGrass(evt.pageX, evt.pageY);
+            me.noticeGrass();
         });
     },
     makeGrass: function(mouseX, mouseY) {
@@ -48,6 +51,33 @@
         grass.style.visibility = 'visible';
 
         this.grassList.push(grass);
+    },
+    noticeGrass: function() {
+        var me = this;
+        if (this.state == 'running-to-rest') {
+            this.state = 'running-to-grass';
+            this.aimFirstGrass();
+        } else if (this.state == 'resting') {
+            this.state = 'heard-grass';
+            setTimeout(function() {
+                me.state = 'running-to-grass';
+                me.aimFirstGrass();
+            }, this.reactGrassTime);
+        }
+    },
+    aimFirstGrass: function() {
+        if (this.grassList.length == 0) {
+            return;
+        }
+
+        var firstGrass = this.grassList[0];
+        var rect = firstGrass.getBoundingClientRect();
+        var fallTop = this.getGrassFallTop(firstGrass);
+
+        this.curTarget = {
+            x: rect.left + rect.width / 2,
+            y: fallTop,
+        };
     },
     makeTarget: function() {
         this.target = document.createElement('div');
@@ -72,9 +102,6 @@
             console.log(me.state);
         }, this.updateRate);
     },
-    updateAgogo: function() {
-        this.moveAgogoPosition();
-    },
     updateTarget: function() {
         this.setElementLeftTop(this.target, this.curTarget.x, this.curTarget.y);
     },
@@ -82,7 +109,7 @@
         var me = this;
         this.grassList.forEach(function(grass) {
             var rect = grass.getBoundingClientRect();
-            var targetTop = parseInt(grass.getAttribute('data-fall-top'));
+            var targetTop = me.getGrassFallTop(grass);
             if (rect.top > targetTop) {
                 return;
             }
@@ -117,22 +144,16 @@
             }
         }
     },
-    moveAgogoPosition: function(rect) {
+    updateAgogo: function(rect) {
         var rect = this.agogo.getBoundingClientRect();
 
         var centerX = rect.left + rect.width / 2;
         var centerY = rect.top + rect.height / 2;
 
-        var distanceX = this.curTarget.x - centerX;
-        var distanceY = this.curTarget.y - centerY;
-        var distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+        var distance = this.calcDistance(this.curTarget.x, this.curTarget.y, centerX, centerY);
 
         if (distance < this.closeDistance) {
-            if (this.state == 'running-to-rest') {
-                this.state = 'resting';
-            } else if (this.state == 'running-to-grass') {
-                this.state = 'eating';
-            }
+            this.onRunningClose();
             return;
         }
 
@@ -147,6 +168,25 @@
         var newTop = rect.top + (this.curTarget.y - centerY) * ratio;
 
         this.setElementLeftTop(this.agogo, newLeft, newTop);
+    },
+    onRunningClose: function() {
+        var me = this;
+        if (this.state == 'running-to-rest') {
+            this.state = 'resting';
+        } else if (this.state == 'running-to-grass') {
+            this.state = 'eating';
+            setTimeout(function() {
+                var oldGrass = me.grassList.shift();
+                document.body.removeChild(oldGrass);
+                if (me.grassList.length == 0) {
+                    me.state = 'running-to-rest';
+                    me.curTarget = me.restPlace;
+                } else {
+                    me.state = 'running-to-grass';
+                    me.aimFirstGrass();
+                }
+            }, this.eatingTime);
+        }
     },
     setAgogoBeginAndRestPosition: function() {
         var place = this.getRandomInt(4);
@@ -192,5 +232,13 @@
     setElementLeftTop: function(element, left, top) {
         element.style.left = left + 'px';
         element.style.top = top + 'px';
+    },
+    getGrassFallTop: function(grass) {
+        return parseInt(grass.getAttribute('data-fall-top'));
+    },
+    calcDistance: function(x1, y1, x2, y2) {
+        var distanceX = x2 - x1;
+        var distanceY = y2 - y1;
+        return Math.sqrt(distanceX * distanceX + distanceY * distanceY);
     },
 }).main();
